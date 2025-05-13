eb603854e670d5bb46903bd475189b907c1fc9e0
glebtyryshkin <glebtyryshkin091@gmail.com>
2025-05-13 22:11:14 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,83 @@
+// books.js
+const books = [
+    {
+        author: 'Александр Пушкин',
+        name: 'Евгений Онегин',
+        publicationYear: 1833,
+        price: 450,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 1869,
+        price: 890,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 550,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Николай Гоголь',
+        name: 'Мёртвые души',
+        publicationYear: 1842,
+        price: 480,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Михаил Лермонтов',
+        name: 'Герой нашего времени',
+        publicationYear: 1840,
+        price: 420,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Антон Чехов',
+        name: 'Вишнёвый сад',
+        publicationYear: 1904,
+        price: 380,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Иван Тургенев',
+        name: 'Отцы и дети',
+        publicationYear: 1862,
+        price: 460,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 670,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Александр Островский',
+        name: 'Гроза',
+        publicationYear: 1859,
+        price: 390,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Александр Грибоедов',
+        name: 'Горе от ума',
+        publicationYear: 1825,
+        price: 410,
+        bestseller: true,
+        inStock: true
+    }
+];
